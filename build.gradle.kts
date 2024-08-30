c551b75be431eca5d0cb85decbd83cf1b6f6bec7
HermitOwO <hermitcutest@gmail.com>
2024-08-30 21:49:52 +0800

    Bump version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ val modId: String = "castirongrill"
 base {
     archivesName.set("CastIronGrill-$minecraftVersion")
     group = "com.hermitowo.castirongrill"
-    version = "2.2"
+    version = "2.3"
 }
 
 java {
